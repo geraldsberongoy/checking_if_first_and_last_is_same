ab08827a8aceb1bbe379a69e67147a417580d502
geraldsberongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2024-01-21 23:46:21 +0800

    Instruction

--- checking_if_first_and_last_is_same.py
+++ checking_if_first_and_last_is_same.py
@@ -2,3 +2,9 @@
 # Write a function to return True if the first and last number of a given list is same. 
 # If numbers are different then return False.
 
+# pseudocode
+# def func
+#   if else statement that print True or False
+# assigning list1
+# assigning list2
+# calling def func
